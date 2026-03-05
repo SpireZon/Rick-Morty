4f0020f8d58a85daabdbd59052e17069b1c09a13
M-Ferrand <Meckonet@protonmail.com>
2026-03-05 16:47:16 +0100

    Ajout des profils des personnages

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -4,15 +4,15 @@ import { useFetch } from '../hooks/usefetch'
 
 function Details() {
     const {id} = useParams()
-    const {data,loading} = useFetch(`https://rickandmortyapi.com/api/character/1`) 
+    const {data,loading} = useFetch(`https://rickandmortyapi.com/api/character/${id}`) 
     console.log(id)
   return (
     <div>
         <img src={data.image} alt="" />
         <div className='Info'>
             <h1>{data.name}</h1>
-            <p><strong>status </strong>{data.status}</p>
-            <p><strong>espèce </strong>{data.species}</p>
+            <p><strong>status : </strong>{data.status}</p>
+            <p><strong>espèce : </strong>{data.species}</p>
             <p><strong>genre </strong>{data.gender}</p>
         </div>
     </div>
--- src/hooks/usefetch.js
+++ src/hooks/usefetch.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 
 export const useFetch = (url) => {
-    const [data, setData] = useState(null)
+    const [data, setData] = useState("")
     const [loading, setLoading] = useState(true)
 
   console.log(url)
@@ -10,14 +10,19 @@ export const useFetch = (url) => {
         setLoading(true)
         try {
             const response = await fetch(url)
+            console.log(response)
             if (!response.ok) throw new Error("Erreur réseau !")
             const json = await response.json()
           console.log()      
         setData(json) 
                                     // !response veux dire reponse barre
         } catch (error) {
-            setLoading(false)
-        }
+            console.log(error)
+        }finally{
+            setLoading(false)}
+            
+            
+        
       }
   
       fetchData();
